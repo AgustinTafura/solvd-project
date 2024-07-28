4a17e8a0000f9e5a11f81fb2827618f5bc05afc3
Agustin Tafura <atafura@the-8agency.com>
2024-07-28 11:03:18 -0300

    jwt added

---
+++ src/jwt.js
@@ -0,0 +1,48 @@
+import { createHmac } from 'crypto';
+
+function base64UrlEncode(str) {
+  return Buffer.from(str)
+    .toString('base64')
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+function base64UrlDecode(str) {
+  str = str.replace(/-/g, '+').replace(/_/g, '/');
+  return Buffer.from(str, 'base64').toString();
+}
+
+export function createJWT(header, payload, secret) {
+  const encodedHeader = base64UrlEncode(JSON.stringify(header));
+  const encodedPayload = base64UrlEncode(JSON.stringify(payload));
+
+  const signature = createHmac('sha256', secret)
+    .update(`${encodedHeader}.${encodedPayload}`)
+    .digest('base64')
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+
+  return `${encodedHeader}.${encodedPayload}.${signature}`;
+}
+
+export function verifyJWT(token, secret) {
+  const [encodedHeader, encodedPayload, signature] = token.split('.');
+
+  const expectedSignature = createHmac('sha256', secret)
+    .update(`${encodedHeader}.${encodedPayload}`)
+    .digest('base64')
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+
+  if (signature !== expectedSignature) {
+    throw new Error('Invalid signature');
+  } 
+
+  const header = JSON.parse(base64UrlDecode(encodedHeader));
+  const payload = JSON.parse(base64UrlDecode(encodedPayload));
+
+  return { header, payload };
+}
