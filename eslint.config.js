29d8d5eea6b421232c5717e68dcf4d0dfa015267
Agustin Tafura <atafura@the-8agency.com>
2024-08-18 23:13:58 -0300

    jest testing env with es6 config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,25 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default [
-	{ languageOptions: { globals: globals.node } },
+	{
+		languageOptions: { globals: globals.node },
+	},
 	pluginJs.configs.recommended,
+	{
+		files: ['**/*.test.js'], 
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+		},
+		plugins: {
+			jest: jestPlugin,
+		},
+		rules: {
+			...jestPlugin.configs.recommended.rules,
+		},
+	},
 ];
